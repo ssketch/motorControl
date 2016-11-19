c93f5bcc8e6e7805f1680130bee35720791af281
Cole Simpson <colesimpson90@gmail.com>
2016-11-18 18:57:36 -0800

    Fixed draw method for 4DOF model

--- MPC/@arm_4DOF/draw.m
+++ MPC/@arm_4DOF/draw.m
@@ -6,26 +6,32 @@ function M = draw(arm)
 [ arm.x, arm.elbow ] = fwdKin( arm, arm.q );
 
 % plot limbs
-plot([arm.shld(1),arm.elbow(1)],[arm.shld(2),arm.elbow(2)],'b',...
-     [arm.elbow(1),arm.x(1)],[arm.elbow(2),arm.x(2)],'c','LineWidth',6);
+plot3([arm.shld(1), arm.elbow(1)], [arm.shld(2), arm.elbow(2)], ...
+      [arm.shld(3), arm.elbow(3)], 'b', 'LineWidth', 6 )
 hold on
+plot3([arm.elbow(1), arm.x(1)], [arm.elbow(2), arm.x(2)], ...
+      [arm.elbow(3), arm.x(3)], 'c', 'LineWidth', 6 )
 
 % plot joints
-plot(arm.shld(1),arm.shld(2),'ko','MarkerSize',20,'MarkerFaceColor','k');
-plot(arm.elbow(1),arm.elbow(2),'ko','MarkerSize',20,'MarkerFaceColor','k');
-plot(arm.x(1),arm.x(2),'ko','MarkerSize',20,'MarkerFaceColor','k');
+plot3( arm.shld(1), arm.shld(2), arm.shld(3), ...
+    'ko', 'MarkerSize', 20, 'MarkerFaceColor', 'k');
+plot3( arm.elbow(1), arm.elbow(2), arm.elbow(3), ...
+    'ko', 'MarkerSize', 20, 'MarkerFaceColor', 'k');
+plot3( arm.x(1), arm.x(2), arm.x(3), ...
+    'ko','MarkerSize',20,'MarkerFaceColor','k');
 
 % set axes to cover workspace (depends on handedness)
-if strcmp(arm.hand,'right')
-    axis([-0.75*arm.l1, arm.l1+arm.l2,...
-          -0.75*arm.l2+0.1, arm.l1+arm.l2+0.1]);
-else
-    axis([-(arm.l1+arm.l2), 0.75*arm.l1,...
-          -0.75*arm.l2+0.1, arm.l1+arm.l2+0.1]);
-end
+% if strcmp(arm.hand,'right')
+%     axis([-0.75*arm.l1, arm.l1+arm.l2,...
+%           -0.75*arm.l2+0.1, arm.l1+arm.l2+0.1]);
+% else
+%     axis([-(arm.l1+arm.l2), 0.75*arm.l1,...
+%           -0.75*arm.l2+0.1, arm.l1+arm.l2+0.1]);
+% end
 
 xlabel('x','FontSize',24);
 ylabel('y','FontSize',24);
+zlabel('z', 'FontSize', 24);
 grid on
 hold off
 
