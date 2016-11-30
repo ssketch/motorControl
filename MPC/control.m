eb865a3acaac8bca4335be8a5e1bb2e818947389
Sean Sketch <sean.sketch@gmail.com>
2016-11-29 23:25:00 -0800

    more changes to control.m

--- MPC/control.m
+++ MPC/control.m
@@ -23,14 +23,14 @@ function u = control(arm, u, ref, space)
 % 
 
 %% LINEARIZATION
-% To linearize the model, compute the 1st-order Taylor series approximation
-% of (1) the dynamics dx/dt = f(x,u) and (2) the output y = g(x), about the
-% current state x_k and control u_k. The dynamics will take the (affine)
-% form dx/dt = f(x,u) = Ax + Bu + c, where A = df/dx and B = df/du. The
-% output equation will take the (affine) form y = g(x) = Cx + d, where C =
-% dg/dx. Both linear approximations are only reasonable for x ?close? to
-% x_k, which we assume to hold over MPC's finite horizon. For example, the
-% linearization of the dynamics proceeds as follows:
+% To linearize the model, we compute the 1st-order Taylor series
+% approximation of (1) dynamics dx/dt = f(x,u) and (2) output y = g(x),
+% about current state x_k and control u_k. The dynamics will take the
+% (affine) form dx/dt = f(x,u) = Ax + Bu + c, where A = df/dx and B =
+% df/du. The output equation will take the (affine) form y = g(x) = Cx + d,
+% where C = dg/dx. Both linear approximations are only reasonable for x
+% "close" to x_k, which we assume to hold over MPC's finite horizon. For
+% example, the linearization of the dynamics proceeds as follows:
 %
 %   f(x,u) = f(x_k,u_k) + df/dx*(x - x_k) + df/du*(u - u_k) + H.O.T.
 %   f(x,u) ~ (df/dx)*x + (df/du)*u + [f(x_k,u_k) - (df/dx)*x_k - (df/du)*u)k]
@@ -57,58 +57,64 @@ B = zeros(nStates,nInputs);
 f = dynamics(arm, arm.q, u);
 for i = 1:nStates
     q_eps = arm.q;
-    q_eps(i) = q_eps(i) + eps;      % 1 state perturbed
-    feps = dynamics(arm, q_eps, u); % state-perturbed dynamics
-    A(:,i) = (feps-f)/eps;
+    q_eps(i) = q_eps(i) + eps;       % 1 state perturbed
+    f_eps = dynamics(arm, q_eps, u); % state-perturbed dynamics
+    A(:,i) = (f_eps-f)/eps;
 end
 
 % compute input-to-state matrix, B
 for i = 1:nInputs
-    u_copy = u;
-    u_copy(i) = u_copy(i) + eps;         % 1 input perturbed
-    feps = dynamics(arm, arm.q, u_copy); % input-perturbed dynamics
-    B(:,i) = (feps-f)/eps;
+    u_eps = u;
+    u_eps(i) = u_eps(i) + eps;           % 1 input perturbed
+    f_eps = dynamics(arm, arm.q, u_eps); % input-perturbed dynamics
+    B(:,i) = (f_eps-f)/eps;
 end
 
-% compute measurement matrix, C
+% compute constant vector, c
+c = f - A*arm.q - B*u;
+
+% compute measurement matrix, C, and constant vector, d
 switch space
     
     % output joint-space state
     case 'joint'
         C = eye(nOutputs,nStates);
+        d = zeros(nOutputs,nInputs);
     
     % output task-space coordinates by linearizing forward kinematics
     case 'cartesian'
+        C = zeros(nOutputs,nStates);
+        g = fwdKin(arm);
+        for i = 1:nStates
+            q_eps = arm.q;
+            q_eps(i) = q_eps(i) + eps;  % 1 state perturbed
+            g_eps = fwdKin(arm, q_eps); % state-perturbed forward kinematics
+            C(:,i) = (g_eps-g)/eps;
+        end
+        d = g - C*arm.q;
         
     % joint space by default
     otherwise
         C = eye(nOutputs,nStates);
+        d = zeros(nOutputs,nInputs);
 end
-k = fwdKin(arm, arm.q );
-for i = 1:nStates
-    
-    % Perturb each state (joint angle) by a small amount
-    qeps = arm.q;
-    qeps(i) = qeps(i) + eps;
-    
-    % Compute the dynamics at the slightly modified state
-    feps = fwdKin( arm, qeps );
-    C(:,i) = (feps - f) ./ eps;
-    
-end
-y = f - C*arm.q;
 
-% Compute the constant term
-c = f - A*arm.q - B*u;
-
-
-% Step 2) Discretize the linearized model with a zero-order hold
-% approximation.
+%% DISCRETIZATION
+% To discretize the model, we use a zero-order hold approximation. This is
+% only applied to the dynamics since the output equation is algebraic, not
+% differential. The derivation is as follows:
+%
+%   dx/dt = Ax + Bu + c
+%   (x+ - x)/Ts = Ax + Bu + c
+%   x+ = (Ax + Bu + c)*Ts + x
+%   x+ = (A*Ts+I)x + (B*Ts)u + (c*Ts)
 
 Ad = A*arm.Ts + eye(size(A));
 Bd = B*arm.Ts;
 cd = c*arm.Ts;
 
+%% STATE AUGMENTATION
+% To account for time delays in the system, 
 %  I'll leave this part for Shaun....
 % % extend state
 % Aext = diag(ones((params.Nstates)*params.numDelSteps_mod,1),-(params.Nstates));
@@ -118,7 +124,7 @@ cd = c*arm.Ts;
 % cext = [c;zeros((params.Nstates)*params.numDelSteps_mod,1)];
 
 
-%% Compute the optimal control for the linearized model
+%% OPTIMIZATION
 % Define the digital linear model, which looks like this:
 %   x_dot = Ax + Bu + f
 %   y = Cx + g
@@ -166,26 +172,6 @@ switch space
         u = ctrl.evaluate( arm.q, 'x.reference', ref);
         
     case 'cartesian'
-
-        %%% Linearize the forward kinematics for tracking
-        % This way we effectively get operational space control, but without
-        % actually having to do any of the math.  This is because the optimal
-        % controller minimizes energetic costs while tracking the end-effector in
-        % cartesian space.
-        
-        f = fwdKin( arm );
-        for i = 1:nStates
-
-                % Perturb each state (joint angle) by a small amount
-                qeps = arm.q;
-                qeps(i) = qeps(i) + eps;
-
-                % Compute the dynamics at the slightly modified state
-                feps = fwdKin( arm, qeps );
-                C(:,i) = (feps - f) ./ eps;
-
-        end
-        y = f - C*arm.q;
 
         % Define the model
         model = LTISystem( 'A', Ad, 'B', Bd, 'f', cd, 'C', C, 'g', y, 'Ts', ...
