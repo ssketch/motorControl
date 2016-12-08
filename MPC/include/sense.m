00f2e323b7b6f10563f72adbecedb938dcd78190
Sean Sketch <sean.sketch@gmail.com>
2016-12-08 13:47:53 -0800

    update naming convention in sense.m

--- MPC/include/sense.m
+++ MPC/include/sense.m
@@ -2,14 +2,14 @@
 % proprioceptive system, accounting for time delay via state augmentation.
 % Note that this sensing is done in joint space regardless of the 'space'
 % set for control, which is only used for the purposes of MPC optimization.
-function y = sense(arm)
+function xSens = sense(arm)
 
 % extract most delayed state from augmented state vector
 nStates = length(arm.x.val);
-y = arm.z.val(end-(nStates-1):end);
+xSens = arm.z.val(end-(nStates-1):end);
 
 % add bias & noise
 sensBias = computeBias(arm);
-y = y + sensBias + arm.sensNoise;
+xSens = xSens + sensBias + arm.sensNoise;
 
 end
