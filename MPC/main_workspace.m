1d5ab559c67aff79308babd7163c25126d568df6
Cole Simpson <colesimpson90@gmail.com>
2016-12-13 12:28:27 -0500

    small update to main_workspace.m

--- MPC/main_workspace.m
+++ MPC/main_workspace.m
@@ -13,16 +13,16 @@ subj.hand = 'right';
 subj.Td = 0;
 subj.coupled = false;
 
-% Define a model.  We'll start with the 2 degree of freedom planar model
-model = arm_4DOF(subj);
-model.draw;
+% Define a arm.  We'll start with the 2 degree of freedom planar arm
+arm = arm_2DOF(subj);
+arm.draw;
 
 
 
 %% Compute workspace:
-% This function computes the workspace of the arm by driving the model arm
+% This function computes the workspace of the arm by driving the arm arm
 % to reach as far as it can in several directions.  We chose to reach
-% towards several points located on a circle of radius, r = 10 m, centered
+% towards several points located on a circle of radius, r = 3 m, centered
 % at the shoulder.
 
 % Specify the radius of the circle designating target locations
@@ -30,11 +30,11 @@ r = 3; % m
 
 % Save the locations of the arm and hand throughout the simulation and the
 % computed control values
-histories.u = zeros(length(model.u.min), 1);
-histories.x = model.x.val;
-histories.y = fwdKin( model );
+histories.u = zeros(length(arm.u.min), 1);
+histories.x = arm.x.val;
+histories.y = fwdKin( arm );
 
-for th = ( 0:30:360 )*pi/180
+for th = ( 0:5:360 )*pi/180
     % Give us a message so we know what's going on
     display(['Reach direction: theta = ' num2str(th*180/pi) 'Deg'])
     display('_________________________________')
@@ -44,28 +44,28 @@ for th = ( 0:30:360 )*pi/180
     % Update the position of the reference
     ref = [ r*cos(th); r*sin(th); 0; 0 ];
 
-    % Reset the model so we start from the same initial posture at the
+    % Reset the arm so we start from the same initial posture at the
     % beginning of each reach.
-    model = arm_4DOF(subj);
+    arm = arm_2DOF(subj);
 
 
     %% Simulate reach.
-    % This while loop simulates a movement until the model stops moving (joint
+    % This while loop simulates a movement until the arm stops moving (joint
     % velocities and accelerations decrease below 1e-2 radians or
     % radians/second) as long as the resulting state is within the joint
     % limits.  This loop also assumes that the movement will take at least 0.1
     % seconds.
-    while model.withinLimits && ( max(abs( x_diff(end,:))) > 1e-2  ...
-            || i*model.Ts < 0.1 )
+    while arm.withinLimits && ( max(abs( x_diff(end,:))) > 1e-2  ...
+            || i*arm.Ts < 0.1 )
 
         % Compute the optimal control value
         try
-            u_star = control( model, i*model.Ts, ref, 'cartesian' );
+            u_opt = control( arm, i*arm.Ts, ref, 'cartesian' );
             % Note: The finite differences method used to linearize the
             % dynamics may cause joint limitation violation warnings even when
             % the actual posture satisfies the constraints.
         catch
-            % The mismatch between linear and nonlinear arm models may cause
+            % The mismatch between linear and nonlinear arm arms may cause
             % infeasible states which then result in errors when computing the
             % next u_star.  This catch function allows us to bypass this
             % problem and proceed with computing the rest of the workspace.
@@ -74,24 +74,23 @@ for th = ( 0:30:360 )*pi/180
             break
         end
 
-        % Implement the optimal torques on the model.
-         plant( model, u_star ); 
-        model.draw;
-
+        % Implement the optimal torques on the arm.
+        zNext = plant(arm, u_opt);
 
         % Sense the resulting sensory outputs and estimate the next state.
-
-
+        x_sens = sense(arm, zNext);
+        arm.draw
+    
         % Save new control, arm configuration, and hand location values
-        histories.u = [ histories.u, u_star ];
-        histories.x = [ histories.x, model.x.val ];
-        histories.y = [ histories.y, fwdKin( model )];
+        histories.u = [ histories.u, u_opt ];
+        histories.x = [ histories.x, arm.x.val ];
+        histories.y = [ histories.y, fwdKin( arm )];
         
         % Let us know how long the simulation is taking and how much things
         % are changing at each step
-        display(['Time = ' num2str(i*model.Ts) 'sec'])
-        display(['Delta q = [' num2str( diff( histories.x')) ...
-            ' ]''' ])
+        display(['Time = ' num2str(i*arm.Ts) 'sec'])
+%         display(['Delta q = [' num2str( diff( histories.x')) ...
+%             ' ]''' ])
         
         % Update simulation time and 
         i = i +1;
@@ -100,17 +99,17 @@ for th = ( 0:30:360 )*pi/180
 end
 
 %% Plot results
-time = linspace( 0, model.Ts*length(histories.u(1,:)), ...
+time = linspace( 0, arm.Ts*length(histories.u(1,:)), ...
     length(histories.u(1,:)));
 figure
 subplot(3,1,1)
     plot( time, histories.u(1,:), 'b', ...
           time, histories.u(2,:), 'r')
       hold on
-      plot( time, ones(size(time))*model.u.min(1), 'b:', ...
-            time, ones(size(time))*model.u.min(2), 'b:', ...
-            time, ones(size(time))*model.u.max(1), 'r:', ...
-            time, ones(size(time))*model.u.max(2), 'r:')
+      plot( time, ones(size(time))*arm.u.min(1), 'b:', ...
+            time, ones(size(time))*arm.u.min(2), 'b:', ...
+            time, ones(size(time))*arm.u.max(1), 'r:', ...
+            time, ones(size(time))*arm.u.max(2), 'r:')
     ylabel 'Optimal joint torques, N-m'
     box off
     
@@ -118,10 +117,10 @@ subplot(3,1,2)
     plot( time, histories.x(1,:)*180/pi, 'b', ...
           time, histories.x(2,:)*180/pi, 'r' )
    hold on
-  plot( time, ones(size(time))*model.thLim(1,1)*180/pi, 'b:', ...
-        time, ones(size(time))*model.thLim(1,2)*180/pi, 'b:', ...
-        time, ones(size(time))*model.thLim(2,1)*180/pi, 'r:', ...
-        time, ones(size(time))*model.thLim(2,2)*180/pi, 'r:')
+  plot( time, ones(size(time))*arm.x.min(1,1)*180/pi, 'b:', ...
+        time, ones(size(time))*arm.thLim(1,2)*180/pi, 'b:', ...
+        time, ones(size(time))*arm.thLim(2,1)*180/pi, 'r:', ...
+        time, ones(size(time))*arm.thLim(2,2)*180/pi, 'r:')
     ylabel 'Joint angle trajecotires, degrees'
     box off
 subplot(3,1,3)
